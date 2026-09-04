d120bd666a581ed76c8803b14e7d10c6caa5c9ba
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2026-09-04 11:09:15 +0500

    backend add

---
+++ backend.py
@@ -0,0 +1,6 @@
+#step1: import database objects
+#step2: cretae FASTAPI app endpoints pseudo code
+#step3: Create Datac contracts using pydantic models
+#step4: Write actual code
+#step5: Create streamlit (just for testing)
+
